51cd08998cb0134fca8e2bd97e5ecbc8296b5fa4
David Shean <dshean@gmail.com>
2023-12-07 18:39:29 -0800

    pdal_transform_gridstats: fixes, update

--- pdal_transform_gridstats.sh
+++ pdal_transform_gridstats.sh
@@ -7,6 +7,9 @@
 #David Shean
 #November 2023
 
+#Edit script for appropraite out_crs, sample_rad_m and run as:
+#pdal_transform_gridstats.sh input.laz | tell > input_pdal.log
+
 #Print commands and exit on error
 set -e -x
 
@@ -35,8 +38,9 @@ bounds=""
 #bounds="([550725.5861, 553620.2589], [5277239.5698, 5279030.06])"
 
 #Thinning - sample point cloud every X m
+sample_rad_m=""
 #pdal translate sample --filters.sample.radius=0.2 --writers.las.forward=all
-sample_rad_m=0.2
+#sample_rad_m=0.2
 
 pdal_filters="--writers.las.forward=all"
 
@@ -46,7 +50,7 @@ pdal_filters="--writers.las.forward=all"
 #Thin point cloud
 if [ ! -z "$sample_rad_m" ] ; then
     laz_out_fn=${laz_out_fn%.*}_${sample_rad_m}m.laz
-    pdal_filters+=" -f filters.sample --filters.sample.radius=$sample_rad_m
+    pdal_filters+=" -f filters.sample --filters.sample.radius=$sample_rad_m"
 fi
 
 #Transform point cloud to out CRS
@@ -66,18 +70,21 @@ if [ ! -z "$bounds" ] ; then
 fi
 
 #Run combined PDAL command
+#Should tee to log?
 eval pdal translate -i $laz_fn -o $laz_out_fn $pdal_filters
 
 #Create rasters
 #Define output grid resolution (meters)
 res=1
 tif_out_fn=${laz_out_fn%.*}_${res}m.tif
-pdal translate -i $laz_out_fn -o ${tif_out_fn%.*}.tif --writers.gdal.resolution=$res --writers.gdal.output_type="all" --writers.gdal.data_type="float32" --writers.gdal.gdalopts="COMPRESS=LZW,TILED=YES,BIGTIFF=IF_SAFER" 
+pdal translate -i $laz_out_fn -o ${tif_out_fn%.*}.tif --writers.gdal.resolution=$res --writers.gdal.output_type="all" --writers.gdal.data_type="float32" --writers.gdal.gdalopts="COMPRESS=LZW,TILED=YES,BIGTIFF=IF_SAFER" --writers.gdal.override_srs=EPSG:$out_crs 
 
 pdal_bands="1 2 3 4 5 6"
 pdal_stats="min max mean idw count stddev"
-#Extract individual statistics
+#Extract individual statistics 
 parallel --delay 1 --link -v "gdal_translate $gdal_opt -b {1} ${tif_out_fn%.*}.tif ${tif_out_fn%.*}_{2}.tif" ::: $pdal_bands ::: $pdal_stats
+#If SRS is not set properly by PDAL on output
+#parallel --delay 1 --link -v "gdal_translate $gdal_opt -b {1} ${tif_out_fn%.*}.tif ${tif_out_fn%.*}_{2}.tif; gdal_edit.py -a_srs EPSG:$out_crs ${tif_out_fn%.*}_{2}.tif" ::: $pdal_bands ::: $pdal_stats
 
 #Shaded relief
 hs.sh ${tif_out_fn%.*}_{max,min,mean,idw}.tif 
